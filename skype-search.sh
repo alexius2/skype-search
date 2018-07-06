552bb60b5129b5180f2ee4fc440d63fc886e85b5
Alexey Ermakov <alexius.work@gmail.com>
2018-07-06 13:21:47 +0600

    add support for searching in selected chats only

--- skype-search.sh
+++ skype-search.sh
@@ -9,13 +9,15 @@ Search specified expression in main.db database
 -B num
     Print num lines of leading context before each match
 -c count
-    Limit number of result lines"
+    Limit number of result lines
+-d dialog
+    Search messages only inside given chat name"
 
 
 OPTIND=1 # Reset in case getopts has been used previously in the shell.
 
 
-while getopts "A:B:c:" opt; do
+while getopts "A:B:c:d:" opt; do
     case $opt in
         A)
             after_context=$OPTARG+1
@@ -25,6 +27,9 @@ while getopts "A:B:c:" opt; do
             ;;
         c)
             limit=$OPTARG
+            ;;
+        d)
+            dialog=$OPTARG
             ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
@@ -54,15 +59,20 @@ fi
 
 query_part="(select displayname from conversations where id = m2.convo_id) || ' | [' || strftime('%Y-%m-%d, %H:%M:%S', datetime(timestamp, 'unixepoch', 'localtime')) || '] ' || from_dispname || ': ' || body_xml as q"
 
+if [ -n "$dialog" ]; then
+    dialog_part=" and convo_id in (select id from conversations where displayname like '%$dialog%')"
+fi
+
+
 if [ -n "$after_context" ] && [ -n "$before_context" ]; then
     echo "not supported yet"
     exit 1
 elif [ -n "$after_context" ]; then
-    query="select (select group_concat((select m3.q), x'0a') from (select $query_part from messages m2 where m2.convo_id = m.convo_id and m2.id >= m.id order by m2.timestamp limit $after_context) m3 ) || x'0a' || '--' from messages m where m.body_xml like '%$@%' order by timestamp"
+    query="select (select group_concat((select m3.q), x'0a') from (select $query_part from messages m2 where m2.convo_id = m.convo_id and m2.id >= m.id order by m2.timestamp limit $after_context) m3 ) || x'0a' || '--' from messages m where m.body_xml like '%$@%' $dialog_part order by timestamp"
 elif [ -n "$before_context" ]; then
-    query="select (select group_concat((select m4.q), x'0a') from (select q from (select $query_part, timestamp from messages m2 where m2.convo_id = m.convo_id and m2.id <= m.id order by m2.timestamp desc limit $before_context) m3 order by timestamp asc) m4 ) || x'0a' || '--' from messages m where m.body_xml like '%$@%' order by timestamp"
+    query="select (select group_concat((select m4.q), x'0a') from (select q from (select $query_part, timestamp from messages m2 where m2.convo_id = m.convo_id and m2.id <= m.id order by m2.timestamp desc limit $before_context) m3 order by timestamp asc) m4 ) || x'0a' || '--' from messages m where m.body_xml like '%$@%' $dialog_part order by timestamp"
 else
-    query="select $query_part from messages m2 where body_xml like '%$@%' order by timestamp"
+    query="select $query_part from messages m2 where body_xml like '%$@%' $dialog_part order by timestamp"
 fi
 
 if [ -n "$limit" ]; then
